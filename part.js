eff3a40e391394f81856554b73c3a25a8c545ffd
Maryam02a <mrymbadrr@gmail.com>
2024-12-14 22:03:01 +0330

    errorHandlers

--- part.js
+++ part.js
@@ -14,16 +14,39 @@ app.get("/:numbers", (req, res) => {
     res.send("main page")
 })
 
-app.use((req, res) => {
-    return res.status(404).json({
-        statuseCode:res.statusCode,
-        error: {
-            type:"not found route",
-            message:"صفحه" + req.url +"یافت نشذ"
+//app.use((req, res) => {
+//    return res.status(404).json({
+//        statuseCode:res.statusCode,
+//        error: {
+//            type:"not found route",
+//            message:"صفحه" + req.url +"یافت نشذ"
+//        }
+//    })
+//})
+
+//app.get("/:numbers", (req, res, next) => {
+//    try{
+//        let number1 = req.params.numbers;
+//        let obj = { key :"" };
+//        console.log(number1);
+//        console.log(obj);
+//
+//        if (number1 == 2) throw { status:400, message:"number is not currect"}
+//
+//        res.send("main page")
+//    } catch (error) {
+//        next (error)
+//    }
+//})
+
+app.use((err, req, res , next) => {
+    return res.status(500).json({
+        statuseCode:res.statuseCode,
+        error:{
+            message:err.message || "InternalServerError"
         }
     })
 })
-
 app.listen(3000, (err) => {
     if(!err){
         console.log("your server is running on port 3000");
