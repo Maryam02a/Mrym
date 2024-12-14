5c21cc9d2403295c1d13b4a51baa5effe186c604
Maryam02a <mrymbadrr@gmail.com>
2024-12-14 22:02:12 +0330

    environment

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
 const express = require("express");
 const app = express();
-
-app.get("/",(req, res) => {
-    res.send("main page")
-    res.status(200).json({
-        statusCode:200,
-        message:"your server is run"
-    })
-});
-
-app.use((req, res, next) => {
-    return status(404).json({
-        statusCode:res.statusCode,
-        error:{
-            type:"not found Rout",
-            message:"صفحه" +req.url + "یافت نشد"
-        }
-    })
+const dotEnv = require(".env");
+const path = require("path");
+dotEnv.config();
+const NodeEnv = process.env.Node_Env;
+dotEnv.config({
+    path: path.join(__dirname, `.env.${NodeEnv}`)
 })
-
-app.listen(3000, (err) => {
-    if(!err) {
-        console.log("your server is running on port 3000");
-    }else{
-        return (err)
-    }
+require("./blogs")
+app.get("/", (req, res, next) => {
+    console.log(process.env.API_KEY);
+    
+    res.send("server run")
+})
+app.listen(process.env.PORT, () => {
+    console.log("server is running on port" + process.env.PORT);
+    
 })
