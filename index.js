0fa64c8158f8dc572b06729c9e40f26bae97652a
Maryam02a <mrymbadrr@gmail.com>
2024-12-24 00:58:04 +0330

    validation

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { AllRoutes } = require("./routers")
-const app = express();
-app.use(AllRoutes)
-
-app.listen(3000, () => {
-    console.log("http://localhost:3000");
-})
