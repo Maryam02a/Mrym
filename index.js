9171c7f6b840806e8399813555e2779ebddaebf2
Maryam02a <mrymbadrr@gmail.com>
2024-12-27 18:30:52 +0330

    pug.part3

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
 const express = require("express");
 const path = require("path");
-const { NotFoundError, ErrorHandller } = require("./util/error-handller");
+const { NotFoundError , ErrorHandller } = require("./util/error-handller")
 const app = express();
-
 
 app.use(express.static("public"))
 
 app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "views"))
+app.set("views", path.join(__dirname, "views"));
 
-app.get("/", (req, res , next) => {
-    res.render("index",{
+app.post("/", (req , res , next) => {
+    const users = [
+        {
+            id:11,
+            name:"Maryam",
+            age:22
+        },
+        {
+            id:12,
+            name:"Mobina",
+            age:19
+        },
+        {
+            id:13,
+            name:"Morteza",
+            age:20
+        },
+    ]
+    res.render("index", {
         link:"https://google.com",
-        section:"this is google program"
+        section:"this is google",
+        users
     })
 })
-
 app.use(NotFoundError)
 app.use(ErrorHandller)
+
 app.listen(3000, () => {
-    console.log("server is running on port 3000 > http://localhost:3000");
+    console.log("server is running > http://localhost:3000");
     
 })
