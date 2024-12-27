f4a09c9eff3d20da11539c900affd7631c9c634f
Maryam02a <mrymbadrr@gmail.com>
2024-12-27 18:18:40 +0330

    pug.part2

--- index.js
+++ index.js
@@ -3,18 +3,22 @@ const path = require("path");
 const { NotFoundError, ErrorHandller } = require("./util/error-handller");
 const app = express();
 
+
 app.use(express.static("public"))
 
-app.set("view engin", "pug");
-app.set("views",path.join(__dirname, "views"))
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"))
 
 app.get("/", (req, res , next) => {
-    res.render("index")
+    res.render("index",{
+        link:"https://google.com",
+        section:"this is google program"
+    })
 })
 
 app.use(NotFoundError)
 app.use(ErrorHandller)
 app.listen(3000, () => {
-    console.log("server is running on port 3000 http://localhost:3000");
+    console.log("server is running on port 3000 > http://localhost:3000");
     
 })
